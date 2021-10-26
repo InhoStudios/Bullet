c5ce20b49c59ca6a335fa58877f18a86afc6e79f
Andy Shen <andyshenproper@gmail.com>
2021-10-26 15:44:27 -0700

    Create basic discord bot functionality

---
+++ app.py
@@ -0,0 +1,19 @@
+import credentials
+import discord
+from icalendar import Calendar, Event
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('Logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
+client.run(credentials.token)
