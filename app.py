744a3f40e7cf0dfb609881efcf1bfc0988b7a734
Andy Shen <andyshenproper@gmail.com>
2021-10-26 17:25:40 -0700

    Receive and parse .ics file

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import credentials
 import discord
 from icalendar import Calendar, Event
+from datetime import datetime
+from pytz import UTC
 
 client = discord.Client()
 
@@ -10,10 +12,36 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-    if message.author == client.user:
+    author = message.author
+    content = message.content
+    chan = message.channel
+    attachments = message.attachments
+
+    if author == client.user:
         return
 
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
+
+    if content.startswith('?'):
+        cmd = content.split('?')[0]
+        if cmd == "free":
+            #TODO: Implement calendar checking
+            print('do something')
+        if cmd == "update":
+            await chan.send("Updating...")
+            events = ""
+            for attachment in attachments:
+                filename = attachment.filename
+                print(filename)
+                if filename.endswith(".ics"):
+                    cal = Calendar.from_ical(await attachment.read())
+                    for component in cal.walk():
+                        if component.name == "VEVENT":
+                            events += component.get("summary") + "\n"
+            await chan.send(events)
+                
+def createCalendar(gcal):
+    for component in gcal.walk():
+        if component.name == "VEVENT":
+            # handle each event
 
 client.run(credentials.token)
